040c597bf76b4bfca22ddf196dbf91935c3ebb85
Paulo Marvin <paulo.marving@gmail.com>
2021-01-11 15:48:09 -0300

    en^?

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
-var speed: float = 350
+
+
+export var speed: float = 300
 var vel := Vector2(0,0)
 
 func _process(delta):
 	pass
+	
+	
 	
 func _physics_process(delta):
 	var dirVec := Vector2(0,0)
@@ -13,6 +17,10 @@ func _physics_process(delta):
 		dirVec.y = -1
 	elif Input.is_action_pressed("move_down"):
 		dirVec.y = 1
+	if Input.is_action_pressed("move_right"):
+		dirVec.x = 1
+	elif Input.is_action_pressed("move_left"):
+		dirVec.x = -1
 	
 	vel = dirVec.normalized() * speed
 	position += vel * delta
@@ -20,3 +28,5 @@ func _physics_process(delta):
 	#This two lines blocks de player to get out the screen
 	var viewRect := get_viewport_rect()
 	position.y = clamp(position.y,0, viewRect.size.y)
+	position.x = clamp(position.x,0, viewRect.size.x)
+	
