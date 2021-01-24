b92dd51413bb328696ba26c97ad8f8891be6c6b2
PauloMarvin <paulo.marving@gmail.com>
2021-01-24 17:43:45 -0300

    meteor script changes

--- Script/Meteor.gd
+++ Script/Meteor.gd
@@ -1,14 +1,19 @@
 extends Area2D
 
-export var minSpeed: float = -50
-export var maxSpeed: float = -10
-export var minRotationRate: float = -40
-export var maxRotationRate: float = 40
+export var minSpeed: float = -100
+export var maxSpeed: float = 100
+export var minRotationRate: float = -75
+export var maxRotationRate: float = 75
+
 
 var speed: float = 0
 var rotationRate: float = 0
 
+var rng = RandomNumberGenerator.new()
+
+
 func _ready():
+	randomize()
 	speed = rand_range(minSpeed,maxSpeed)
 	rotationRate = rand_range(minRotationRate,maxRotationRate)
 	
@@ -16,3 +21,7 @@ func _physics_process(delta):
 	rotation_degrees += rotationRate * delta
 	position.x  += speed * delta
 	position.y  += speed * delta
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
