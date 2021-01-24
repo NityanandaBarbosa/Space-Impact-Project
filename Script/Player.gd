de009442fb3602b861796a2b6e49d73b35a4a0ce
Nityananda Barbosa <nityanandateixeira@great.ufc.br>
2021-01-24 17:22:16 -0300

    feat : player shooting by the normal gun

--- Script/Player.gd
+++ Script/Player.gd
@@ -4,14 +4,20 @@ var plBullet := preload("res://Scenes/Player/bullet.tscn")
 
 export var speed: float = 300
 
+onready var normalGun := $FireGun
+onready var gunsPosition := $SpecialGuns
+
 var vel := Vector2(0,0)
 
 func _process(delta):
 	if Input.is_action_pressed("shoot"):
-		var bulllet = plBullet.instance()
-		bulllet.position = position
+		var bulllet := plBullet.instance()
+		bulllet.global_position = normalGun.global_position
 		get_tree().current_scene.add_child(bulllet)
-	
+		#for child in gunsPosition.get_children():
+		#	var bulllet := plBullet.instance()
+		#	bulllet.global_position = child.global
+		#	get_tree().current_scene.add_child(bulllet)
 	
 func _physics_process(delta):
 	var dirVec := Vector2(0,0)
