6ecdf0238527ddabff68fbc17141c77d18a699a5
Nityananda Barbosa <nityanandateixeira@great.ufc.br>
2021-02-13 09:49:22 -0300

    feat: Meteor Damage

--- Script/Meteor.gd
+++ Script/Meteor.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
-export var minSpeed: float = -100
-export var maxSpeed: float = 100
-export var minRotationRate: float = -75
-export var maxRotationRate: float = 75
+export var minSpeed: float = -300
+export var maxSpeed: float = -100
+export var minRotationRate: float = -255
+export var maxRotationRate: float = 255
 
+export var life: int = 20
 
 var speed: float = 0
 var rotationRate: float = 0
@@ -20,8 +21,12 @@ func _ready():
 func _physics_process(delta):
 	rotation_degrees += rotationRate * delta
 	position.x  += speed * delta
-	position.y  += speed * delta
+	#position.y  += speed * delta
 
+func damage(amount: int):
+	life -= amount
+	if life <= 0:
+		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
