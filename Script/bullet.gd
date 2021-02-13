ab4a3e98c572fba7c4e7d61096aa39698db275b1
Guilherme Portela <guilhermemarquesportela@gmail.com>
2021-02-13 09:15:58 -0300

    updating here

--- Script/bullet.gd
+++ Script/bullet.gd
@@ -5,5 +5,5 @@ export var speed: float = 40
 func _physics_process(delta):
 	position.x += speed + delta
 	
-#func _on_VisibilityNotifier2D_screen_exited():
-#	queue_free()
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
