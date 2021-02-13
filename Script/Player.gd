05c8a9a80bf7479f96d1ea9bd95bbaaeeb963138
Guilherme Portela <guilhermemarquesportela@gmail.com>
2021-02-13 11:19:56 -0300

    feat:first audio stage

--- Script/Player.gd
+++ Script/Player.gd
@@ -13,11 +13,13 @@ onready var fireDelayTimer := $FireDelayerTimer
 var vel := Vector2(0,0)
 
 func _process(delta):
+	
 	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
 		fireDelayTimer.start(fireDelay)
 		var bulllet := plBullet.instance()
 		bulllet.global_position = normalGun.global_position
 		get_tree().current_scene.add_child(bulllet)
+		$sfx_shot.play()
 		#for child in gunsPosition.get_children():
 		#	var bulllet := plBullet.instance()
 		#	bulllet.global_position = child.global
