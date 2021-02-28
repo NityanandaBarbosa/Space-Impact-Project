835e1ba42ba8a814a3a4dc9ba6bdbbe3f98f080f
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2021-02-28 19:08:20 -0300

    feat: while de game is open, system may record high score

--- Script/Global.gd
+++ Script/Global.gd
@@ -3,6 +3,7 @@ extends Node
 var score : int = 0
 var control_shot = false
 export var life: int = 3
+var highScore = 0
 
 func _ready():
 	pass 
@@ -10,6 +11,11 @@ func _ready():
 func _enemykilled(points):
 	score += points
 
+func _check_high_score():
+	if(score > highScore):
+		highScore = score
+
 func _reset_values():
-	control_shot = false
+	_check_high_score()
 	life = 3
+	score = 0
